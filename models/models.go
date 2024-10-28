25db98c70272107e48634f5cf3068f5795c6a0bf
MINSEO JEONG <37695295+m1ns2o@users.noreply.github.com>
2024-10-29 01:28:02 +0900

    foreignkey cascade

--- models/models.go
+++ models/models.go
@@ -5,11 +5,10 @@ import (
 )
 
 type Class struct {
-	ID       uint   `gorm:"primaryKey"`
-	Classnum string `gorm:"unique;varchar(100)"`
-	Passwd   string `gorm:"type:varchar(100)"`
-	Problems []Problem
-	// Teachers []Teacher
+	ID       uint      `gorm:"primaryKey"`
+	Classnum string    `gorm:"unique;type:varchar(100)"` // 명확한 길이 지정
+	Passwd   string    `gorm:"type:varchar(100)"`
+	Problems []Problem `gorm:"constraint:OnDelete:CASCADE"`
 }
 
 type Problem struct {
@@ -18,22 +17,19 @@ type Problem struct {
 	Content        string `gorm:"type:varchar(500)"`
 	TestcaseInput  string `gorm:"type:varchar(100)"`
 	TestcaseOutput string `gorm:"type:varchar(100)"`
-	ClassID        uint
-	// Class          Class `gorm:"foreignKey:ClassID"`
-	// SolvedProblems []Solved
+	ClassID        uint   `gorm:"foreignKey:ClassID;references:ID"`
 }
 
 type User struct {
 	ID       uint   `gorm:"primaryKey"`
 	Name     string `gorm:"type:varchar(50)"`
-	Classnum string `gorm:"type:string;foreignKey:Classnum;references:Classnum"`
-	// Solved   []Solved
+	Classnum string `gorm:"type:varchar(100)"` // 명확한 길이 지정
 }
 
 type Solved struct {
 	ID        uint      `gorm:"primaryKey"`
 	ProblemID uint      `gorm:"foreignKey:ProblemID;references:ID"`
 	UserID    uint      `gorm:"foreignKey:UserID;references:ID"`
-	UserName  string    `gorm:"foreignKey:UserName;references:UserName;type:varchar(50)"`
+	UserName  string    `gorm:"type:varchar(50)"`
 	SolvedAt  time.Time `gorm:"autoCreateTime"`
 }
